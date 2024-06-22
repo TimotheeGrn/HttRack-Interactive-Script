191562d3ac2ab7f37a33d46c564550b1c180fb91
TimotheeGrn <121766524+TimotheeGrn@users.noreply.github.com>
2024-06-22 23:42:10 +0200

    Update main.py
    Amélioration du script

--- main.py
+++ main.py
@@ -12,23 +12,36 @@ try:
     | |  | |__| |      | |    | |  | |_| |_ 
     |_|   \____/       |_|    |_|  |_|_____|
                                             """, "green"))
-        print(colored("                             by Yueag", "red"))
-        url = input(colored("\n\n[YU-PHI] - Please enter a url: ", "yellow"))
+        print(colored("                             by the PTCW Team", "red"))
+        url = input(colored("\n\n[YU-PHI] - Please enter a URL (including http:// or https://): ", "yellow"))
+        
+        # Verif de l'url
+        parsed_url = urlparse(url)
+        if not parsed_url.scheme:
+            print(colored("[YU-PHI] - Invalid URL. Please include the scheme (http:// or https://)", "red"))
+            continue
+
         if not os.path.exists("websites"):
             os.makedirs("websites")
+        
         os.system('clear')
         print(colored(f"[YU-PHI] - Target = {url}\n", "blue"))
-        domain = urlparse(url).netloc
-        os.chdir("websites")
-        os.mkdir(domain)
-        os.chdir(domain)
+        
+        domain = parsed_url.netloc
+        if not domain:
+            print(colored("[YU-PHI] - Invalid URL. Could not parse domain.", "red"))
+            continue
+        
+        # Créer le path du site web
+        path = f"websites/{domain}/"
+        os.makedirs(path, exist_ok=True)
+        
+        os.chdir(path)
         print(colored("[YU-PHI] - Your website is cloning...\n", "yellow"))
+        
+        # Lancer httrack
         subprocess.run(["httrack", "--mirror", url])
-        print(colored("\n[YU-PHI] - A php server will be run...\n", "yellow"))
-        print(colored("[YU-PHI] - Press [CTRL]+[C] to stop the php server...", "yellow"))
-        subprocess.run(["php", "-S", "localhost:8000"])
-
-        pass
+        print(colored(f"\n[YU-PHI] - {url} has been cloned to the directory {path}\n", "yellow"))
+        break
 except KeyboardInterrupt:
     print(colored("\n[YU-PHI] - Program is finish.", "red"))
-
