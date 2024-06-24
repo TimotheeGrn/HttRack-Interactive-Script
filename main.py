c85f97f085c3d90eeb374b1d87b784c79b571fd7
TimotheeGrn <121766524+TimotheeGrn@users.noreply.github.com>
2024-06-24 23:25:53 +0200

    Update main.py
    A better script

--- main.py
+++ main.py
@@ -5,31 +5,31 @@ from termcolor import colored
 
 try:
     while True:
-        print(colored(""" __     ___    _        _____  _    _ _____ 
- \ \   / / |  | |      |  __ \| |  | |_   _|
-  \ \_/ /| |  | |______| |__) | |__| | | |  
-   \   / | |  | |______|  ___/|  __  | | |  
-    | |  | |__| |      | |    | |  | |_| |_ 
-    |_|   \____/       |_|    |_|  |_|_____|
-                                            """, "green"))
-        print(colored("                             by the PTCW Team", "red"))
-        url = input(colored("\n\n[YU-PHI] - Please enter a URL (including http:// or https://): ", "yellow"))
+        print(colored("""'
+ ||'  '||' '||'  .|'''.|  
+ ||    ||   ||   ||..  '  
+ ||''''||   ||    ''|||.  
+ ||    ||   ||  .     '|| 
+.||.  .||. .||. |'....|'
+HttRack Interactive Script""", "green"))
+        print(colored("                            by the PTCW Team", "red"))
+        url = input(colored("\n\n[HIS] - Please enter a URL (including http:// or https://): ", "yellow"))
         
         # Verif de l'url
         parsed_url = urlparse(url)
         if not parsed_url.scheme:
-            print(colored("[YU-PHI] - Invalid URL. Please include the scheme (http:// or https://)", "red"))
+            print(colored("[HIS] - Invalid URL. Please include the scheme (http:// or https://)", "red"))
             continue
 
         if not os.path.exists("websites"):
             os.makedirs("websites")
         
         os.system('clear')
-        print(colored(f"[YU-PHI] - Target = {url}\n", "blue"))
+        print(colored(f"[HIS] - Target = {url}\n", "blue"))
         
         domain = parsed_url.netloc
         if not domain:
-            print(colored("[YU-PHI] - Invalid URL. Could not parse domain.", "red"))
+            print(colored("[HIS] - Invalid URL. Could not parse domain.", "red"))
             continue
         
         # Créer le path du site web
@@ -37,11 +37,11 @@ try:
         os.makedirs(path, exist_ok=True)
         
         os.chdir(path)
-        print(colored("[YU-PHI] - Your website is cloning...\n", "yellow"))
+        print(colored("[HIS] - Your website is cloning...\n", "yellow"))
         
         # Lancer httrack
         subprocess.run(["httrack", "--mirror", url])
-        print(colored(f"\n[YU-PHI] - {url} has been cloned to the directory {path}\n", "yellow"))
+        print(colored(f"\n[HIS] - {url} has been cloned to the directory {path}\n", "yellow"))
         break
 except KeyboardInterrupt:
-    print(colored("\n[YU-PHI] - Program is finish.", "red"))
+    print(colored("\n[HIS] - Program is finish.", "red"))
